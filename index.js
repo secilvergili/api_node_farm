307062fb59660a107559ec057ba73ab61a25e26a
secilvergili <secilvergili@gmail.com>
2025-06-10 20:40:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const replaceTemplate = require("./modules/replaceTemplate.js");
+
+// önce şablonları okuyalım ki yazı verisi olarak kullanıcıya gönderebilelim.
+let overviewHTML = fs.readFileSync("./templates/overview.html", "utf-8");
+let detailTemplate = fs.readFileSync("./templates/product.html", "utf-8");
+let cardTemplate = fs.readFileSync("./templates/card.html", "utf-8");
+
+let jsonData = fs.readFileSync("./dev-data/data.json", "utf-8");
+
+const data = JSON.parse(jsonData);
+
+const server = http.createServer((request, response) => {
+
+
+  const { query, pathname } = url.parse(request.url, true)
+
+  switch (pathname) {
+
+    case "/overview":
+     const cards = data.map( (item) => replaceTemplate(cardTemplate, item));
+      overviewHTML = overviewHTML.replace("{%PRODUCT_CARDS%}"/g, cards.join(""));
+      return response.end(overviewHTML);
+      
+    case "/product":
+    const item = data.find((item) => item.id == query.id);
+    const output = replaceTemplate(detailTemplate, item);
+
+   
+
+      return response.end(output);
+
+    default:
+      return response.end("<h1>404 <h2> Undefined Path</h2> Not Found</h1>");
+  }
+
+ 
+});
+// oluşturulan server değişkeni ile sunucuyu başlatalım.
+server.listen(4000, "127.0.0.1", () => {
+  console.log("Server is listening on port 4000");
+});
---
+++ modules/replaceTemplate.js
@@ -0,0 +1,23 @@
+
+
+const replaceTemplate = (html, data) => {
+
+let output = html.replace(/{%PRODUCTNAME%}/g, data.productName);
+          output = output.replace(/{%IMAGE%}/g, data.image);
+          output = output.replace(/{%PRICE%}/g, data.price);
+          output = output.replace(/{%FROM%}/g, data.from);
+          output = output.replace(/{%NUTRIENTS%}/g, data.nutrients);
+          output = output.replace(/{%QUANTITY%}/g, data.quantity);
+          output = output.replace(/{%DESCRIPTION%}/g, data.description);
+          output = output.replace(/{%ID%}/g, data.id);
+          
+          if (!data.organic){
+            output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+          }else{
+            output = output.replace(/{%NOT_ORGANIC%}/g, "");
+          }
+          return output;
+          
+};
+
+module.exports = replaceTemplate;
