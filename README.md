77f98c10212c72807f46c57c19c7cbd7987da727
secilvergili <secilvergili@gmail.com>
2025-06-12 13:17:10 +0200

    Local changes before pulling from GitHub

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # api_node_farm
+
+
+This project is a simple Node.js HTTP server built without any external frameworks, to demonstrate how routing, file handling, and templating work at a low level.
+
+I used the built-in http, url, and fs modules. When a request is received, I parse the URL to get the pathname and query parameters. Based on the pathname, the server responds with either an overview page that dynamically loads product cards, or a detailed product page if an ID is provided.
+
+The HTML templates are read from the file system, and I use a custom replaceTemplate function to inject product data into placeholders. If the route doesn’t match any known path, a 404 message is returned. This project helped me deeply understand routing logic and template rendering from scratch, without Express or other abstractions.
+
+http://127.0.0.1:4000/overview
+
--- index.js
+++ index.js
@@ -3,37 +3,36 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-
-const replaceTemplate = require("./modules/replaceTemplate.js");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 // önce şablonları okuyalım ki yazı verisi olarak kullanıcıya gönderebilelim.
 let overviewHTML = fs.readFileSync("./templates/overview.html", "utf-8");
 let detailTemplate = fs.readFileSync("./templates/product.html", "utf-8");
 let cardTemplate = fs.readFileSync("./templates/card.html", "utf-8");
-
 let jsonData = fs.readFileSync("./dev-data/data.json", "utf-8");
-
 const data = JSON.parse(jsonData);
 
 const server = http.createServer((request, response) => {
-
-
-  const { query, pathname } = url.parse(request.url, true)
+  // url = product?id=0,1,2,...
+  // pathname= "/product"
+  // const query = { id: "0" };
+  const {pathname, query} = url.parse(request.url, true)
+  console.log("Request received.", pathname);
 
   switch (pathname) {
 
     case "/overview":
-     const cards = data.map( (item) => replaceTemplate(cardTemplate, item));
-      overviewHTML = overviewHTML.replace("{%PRODUCT_CARDS%}"/g, cards.join(""));
-      return response.end(overviewHTML);
+     const cards = data.map(item => replaceTemplate(cardTemplate, item));
+      finalOverview = overviewHTML.replace(/{%PRODUCT_CARDS%}/g, cards.join(""));
+      return response.end(finalOverview);
       
     case "/product":
-    const item = data.find((item) => item.id == query.id);
+    const item = data.find(item => item.id == query.id);
     const output = replaceTemplate(detailTemplate, item);
-
-   
-
       return response.end(output);
+
+      case "/favicon.ico":
+        return response.end();
 
     default:
       return response.end("<h1>404 <h2> Undefined Path</h2> Not Found</h1>");
